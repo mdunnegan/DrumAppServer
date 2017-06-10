8c7209710649d2baebfefd78efbe2695f541cf1b
Michael Dunnegan <mdunnegan@spicy.io>
2017-06-10 12:06:08 -0400

    Fixes...

---
+++ config.js
@@ -0,0 +1,6 @@
+// Hold application secrets
+
+module.exports = {
+	secret: 'dsf334312gdgfsdfcgegsaf'
+  database: 'mongodb://heroku_vcn81hf6:tss2u01oh0d5dorfn48fv0sujs@ds119772.mlab.com:19772/heroku_vcn81hf6'
+}
